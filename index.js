ebe3b95683b634e5b76b726f42323df5ccc727ca
ashma4444 <ashma20570218@gmail.com>
2023-08-23 14:40:24 +0545

    learned express router

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 
 const app = express();
+const indexRouter = require("./routes"); // index.js mention nagere pani automatically index.js lai khojcha
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World");
+// });
+
+app.use("/", indexRouter);
 
 app.listen(3333); // above 3000 we can use any port
 console.log("App is running on port 3333");
